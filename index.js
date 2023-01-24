fbcfe11c4da265a972025520cde26843d6b1d068
Giacomo <giax.95@live.it>
2023-01-24 14:52:01 +0100

    Consegna primo esercizio.

---
+++ index.js
@@ -0,0 +1,27 @@
+// Esercizio 1
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  compareAge(otherPerson) {
+    if (this.age > otherPerson.age) {
+      return `${this.name} è più vecchio di ${otherPerson.name}.`;
+    } else if (this.age < otherPerson.age) {
+      return `${this.name} è più giovane di ${otherPerson.name}.`;
+    } else {
+      return `${this.name} ha la stessa età di ${otherPerson.name}.`;
+    }
+  }
+}
+
+const p1 = new Person("Mario", 30);
+const p2 = new Person("Paolo", 25);
+const p3 = new Person("Giovanni", 30);
+
+console.log(p1.compareAge(p2));
+console.log(p2.compareAge(p1));
+console.log(p1.compareAge(p3));
+
+// Esercizio 2
